c329572d7b941818dc47ffc03dc8d1d1e628d43a
Md Tareque Khan <mdtareque@users.noreply.github.com>
2014-09-15 13:02:19 +0530

    Update javaMemoryIntensive.java

--- tech_notes_n_quick_ref_codes/javaMemoryIntensive.java
+++ tech_notes_n_quick_ref_codes/javaMemoryIntensive.java
@@ -26,3 +26,48 @@ public class MemoryIntensive {
 	}
 
 }
+
+
+/*
+
+OOM test started..
+
+Iteration 1 Free Mem: 1310461776
+
+Required Memory for next loop: 100
+Iteration 2 Free Mem: 1310461776
+
+Required Memory for next loop: 500
+Iteration 3 Free Mem: 1310461776
+
+Required Memory for next loop: 2500
+Iteration 4 Free Mem: 1310461776
+
+Required Memory for next loop: 12500
+Iteration 5 Free Mem: 1310461776
+
+Required Memory for next loop: 62500
+Iteration 6 Free Mem: 1310461776
+
+Required Memory for next loop: 312500
+Iteration 7 Free Mem: 1310461776
+
+Required Memory for next loop: 1562500
+Iteration 8 Free Mem: 1310461776
+
+Required Memory for next loop: 7812500
+Iteration 9 Free Mem: 1304211760
+
+Required Memory for next loop: 39062500
+Iteration 10 Free Mem: 1272961744
+
+Required Memory for next loop: 195312500
+Iteration 11 Free Mem: 1116711728
+
+Required Memory for next loop: 976562500
+Iteration 12 Free Mem: 335461712
+Exception in thread "main" java.lang.OutOfMemoryError: Java heap space
+        at MemoryIntensive.generateOOM(MemoryIntensive.java:15)
+        at MemoryIntensive.main(MemoryIntensive.java:5)
+
+*/
