0f7c7e2d8b6bc1fef84e283ea3de3b5caf31d416
Md Tareque Khan <mdtareque@users.noreply.github.com>
2014-09-29 20:05:28 +0530

    Create a2p1.c

---
+++ iitk_cs_101/a2/a2p1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int divide(int a,int b)
+{
+    if(b==0 || a%b!=0)
+        return -1;
+    return a/b;
+}
+int main()
+{
+    int a,b,d;
+    scanf("%d%d",&a,&b);
+    d=divide(a,b);
+    printf("%d",d);
+    return 0;
+}
